dc803b199c12166f5ebffe0b69d552eba1702c7a
NayoungKim1212 <kkd04250@naver.com>
2023-09-20 19:18:23 +0900

    Feat 피드마다 각각 다른 이미지 보여주기
    피드마다 각각 다른 이미지 보여주기

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Feed extends StatelessWidget {
+class Feed extends StatefulWidget {
   const Feed({
-    super.key,
-  });
+    Key? key,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  final String imageUrl; // 이미지를 담을 변수
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  // 좋아요 여부
+  bool isFavorite = false;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +27,7 @@ class Feed extends StatelessWidget {
           borderRadius: BorderRadius.circular(8),
           // 이미지
           child: Image.network(
-            'https://cdn2.thecatapi.com/images/6bt.jpg',
+            widget.imageUrl, // 10번째 줄의 imageUrl 가져오기
             width: 100,
             height: 100,
             fit: BoxFit.cover,
@@ -59,12 +70,19 @@ class Feed extends StatelessWidget {
                   Spacer(), // 자기가 차지할수 있는 공간 최대한 차지하는 나머지 요소들을 밀어내는 위젯
                   GestureDetector(
                     // 아이콘을 눌렀을 때 특정 동작을 하게끔 제스처들을 인식
-                    onTap: () {},
+                    onTap: () {
+                      // 화면 갱신
+                      setState(() {
+                        isFavorite = !isFavorite; // 좋아요 토글
+                      });
+                    },
                     child: Row(
                       children: [
                         Icon(
-                          CupertinoIcons.heart,
-                          color: Colors.black54,
+                          isFavorite
+                              ? CupertinoIcons.heart_fill
+                              : CupertinoIcons.heart,
+                          color: isFavorite ? Colors.pink : Colors.black,
                           size: 16,
                         ),
                         Text(
